2d3d3de578f50f2b478b0ae399728369a6f49271
thayna regina manoel <thaynareginam@hotmail.com>
2021-09-10 21:45:44 -0300

    editanod-como-pedi-o-passo-a-passo

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,83 @@
+/* Exercícios de interpretação de código 
+
+QUESTÃO 1 
+ 
+a.FALSE
+b.FALSE
+c.TRUE
+d.boolean
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+QUESTÃO 2 
+
+// Como meu objtivo e calcular os valores , sera nescessario usar uma conversao de string para number .
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+QUESTÃO 3
+
+
+let primeiroNumero = Number (prompt ("Digite um numero!"))
+let segundoNumero = Number (prompt ("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+ */
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+/* Exercícios de escrita de código 
+
+ QUESTÃO 1 
+
+
+const idadeDoUsuario = Number (prompt ("qual sua idade?"))
+const idadeMelhorAmigo = Number (prompt ("qual a idade do seu melhor amigo?"))
+
+console.log ("Sua idade é maior do que a do seu melhor amigo",idadeDoUsuario > idadeMelhorAmigo)
+console.log ("qual a diferença de idade ?",idadeDoUsuario-idadeMelhorAmigo)
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+/*   QUESTÃO 2 
+
+const NumeroPar = Number (prompt ("insira um numero par?"))
+
+console.log( NumeroPar % 2 )
+
+// c)Percebi que ao adicionar um numero qualquer par, sempre me retornava o mesmo resultado 0, pois o numero 2 e divisivel com qualquer numero par . 
+
+// d) quando inseri um numero impar o console me retornou o resto da divisao por nao ter dado um numero iteiro.
+
+
+//-------------------------------------------------------------------------------------------------------------------------------- 
+
+// QUESTÃO 3 
+
+     const SuaIdade = Number (prompt ("digite a sua idade?"))
+     console.log ("a sua idade em meses:",SuaIdade*12)
+     console.log ("a sua idade em dias:", SuaIdade*365)
+     console.log ("a sua idade em horas:", SuaIdade*8760)
+
+//-------------------------------------------------------------------------------------------------------------------------------- 
+
+/* // QUESTÃO 4  */
+/* 
+const primeiroNumero = Number (prompt ("digite um numero"))
+const segundoNuemero = Number (prompt ("digite outro numero?"))
+
+console.log ("O primeiro numero é maior que segundo?",primeiroNumero > segundoNuemero)
+console.log ("O primeiro numero é igual ao segundo?",primeiroNumero == segundoNuemero)
+console.log ("O primeiro numero é divisível pelo segundo?",primeiroNumero % segundoNuemero ===0)
+console.log ("O segundo numero é divisível pelo primeiro?",primeiroNumero % segundoNuemero ===0) */
+
+
+//-------------------------------------------------------------------------------------------------------------------------------- 
+
+
+
+
+
