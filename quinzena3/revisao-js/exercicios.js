d9ee3f77d1dbe80684100a534af930034e1eedb0
thayna regina manoel <thaynareginam@hotmail.com>
2021-10-10 16:44:11 -0300

    editando exercicios

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -34,9 +34,9 @@ return numerosPares
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
-  let maiorNumero = array[0]
+ let maiorNumero = array[0]
   for(let i = 0; i < array.length; i++){
-    let numeroDaVez = numeros[i]
+    let numeroDaVez = array[i]
     if(numeroDaVez > maiorNumero){
       maiorNumero = numeroDaVez
     
@@ -47,24 +47,54 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
-  
-}
+const valorElementos = array.length
+return valorElementos
+}  
+
+
+
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+ const booleano1 = true
+ const booleano2 = false
+ const booleano3 = !booleano2 
+ const booleano4 = !booleano3 
+
+ const respostas = [(booleano1 && booleano2 && !booleano4), ((booleano1 && booleano2) || !booleano3),(booleano2 || booleano3) && (booleano4 || booleano1),!(booleano2 && booleano3) || !(booleano1 && booleano3),!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)]
+
+ return respostas
 
 }
+
 
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
+let numerosPares = []
+for(let i = 0; i<n; i++){
 
+   numerosPares.push(i*2)
+   
+ }
+ return numerosPares
 }
 
 // EXERCÍCIO 08
-function checaTriangulo(a, b, c) {
-  // return 'Escaleno'
-  // return 'Equilátero'
-  // return 'Isósceles'
+
+
+  function checaTriangulo(a, b, c) {
+    // return 'Escaleno'
+    // return 'Equilátero'
+    // return 'Isósceles'
+  
+  
+      if(a === b && a === c){
+       return 'Equilátero'
+      }else if(a !== b && a !==c && b !== c){
+       return  "Escaleno"
+      } else((a=== b && b !== c) || (a !== b && b === c))
+       return 'Isósceles'
+
 }
 
 // EXERCÍCIO 09
@@ -75,38 +105,108 @@ function comparaDoisNumeros(num1, num2) {
   //   maiorDivisivelPorMenor: Y,
   //   diferenca: Z
   // }
+  let maior = undefined
+  let menor = undefined
+  let maiorDivisivelPorMenor = undefined
+
+  if (num1 > num2) {
+    maior = num1
+    menor = num2
+  } else if (num2 > num1) {
+    maior = num2
+    menor = num1
+  } else if (num1 === num2) {
+    maior = num1
+    menor = num1
+  }
+  if (maior % menor === 0) {
+    maiorDivisivelPorMenor = true
+  } else {
+    maiorDivisivelPorMenor = false
+  }
+    
+    const diferenca = maior - menor
+
+    return {maiorNumero : maior, maiorDivisivelPorMenor : maiorDivisivelPorMenor, diferenca : diferenca}
+    
 }
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
+  let novaArray = []
 
+  for(item of array){
+    let contador = 0
+    for (compradoItem of array){
+      if(item>compradoItem){
+      contador++
+      
+    }
+  }
+  novaArray[contador] = item
+}
+let arrayDosSegundos = [novaArray[novaArray.length-2],(novaArray[1])]
+return arrayDosSegundos
 }
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
 
+const novoArray = array.sort((a, b)=>{
+
+return a - b
+    
+  })
+    
+return novoArray
+
 }
 
 // EXERCÍCIO 12
 function filmeFavorito() {
+  const filmeFavorito = {
+    nome: 'O Diabo Veste Prada',
+    ano: 2006,
+    diretor: 'David Frankel',
+    atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+
+  }
+    return filmeFavorito
 
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  const filmeFavorito = {
+    nome: 'O Diabo Veste Prada',
+    ano: 2006,
+    diretor: 'David Frankel',
+    atores: ['Meryl Streep', ' Anne Hathaway', ' Emily Blunt', ' Stanley Tucci']
+
+  }
+ return`Venha assistir ao filme ${filmeFavorito.nome}, de ${filmeFavorito.ano}, dirigido por ${filmeFavorito.diretor} e estrelado por ${filmeFavorito.atores}.`
+
 }
+
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
+  const medidasRetangulo = {
+    largura: lado1,
+    altura: lado2,
+    perimetro: (2 * (lado1 + lado2)) ,
+    area: (lado1 * lado2)
 
+  }
+return medidasRetangulo
 }
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
-
+ const pessoaAnonima = {...pessoa, nome:"ANÔNIMO"}
+ return pessoaAnonima
 }
-
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
 
