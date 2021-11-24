0033c3e04f660be1032a22a0c5c8594209a4fb3e
thayna regina manoel <thaynareginam@hotmail.com>
2021-11-24 00:12:28 -0300

    editando

--- modulo3/rend-condicional-form/labenuform/src/components/Etapa3.js
+++ modulo3/rend-condicional-form/labenuform/src/components/Etapa3.js
@@ -4,14 +4,15 @@ class Etapa3 extends React.Component {
     render() {
       return (
         <div>
-        <h2>ETAPA 3 - INFORMAÇOES GERAUS DE ENSINO</h2>
+        <h2>ETAPA 3 - INFORMAÇOES GERAIS DE ENSINO</h2>
         <p>Por que você não terminou o curso de graduação?</p>
         <input/>
         <p>Você fez algum curso complementar?</p>
         <select>
             <option>Nenhum</option>;
-            <option>Curso técnico</option>;
-            <option>Cursp de inglês</option>;
+            <option>graduação</option>;
+            <option>tecnologo</option>;
+            <option>Curso tecnico</option>;
         </select>
         </div>
       );
