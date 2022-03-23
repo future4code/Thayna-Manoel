3a857fb7a0933a2f327dfa13dd8d6f1f973b12ea
thaynareginam <thaynareginam@hotmail.com>
2022-03-22 23:21:53 -0300

    editando exercicios

---
+++ modulo6/intro-typescript/src/exericio1.ts
@@ -0,0 +1,11 @@
+// Exercício 1  
+
+function checaTriangulo(a: number , b: number, c: number): string {
+  if (a !== b && b !== c) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
+}
---
+++ modulo6/intro-typescript/src/exericio5.ts
@@ -0,0 +1,23 @@
+function checaRenovacaoRG() {
+  const anoAtual = Number(prompt("Digite o ano atual"))
+  const anoNascimento = Number(prompt("Digite o ano de nascimento"))
+  const anoEmissao = Number(prompt("Digite o ano de emissão do documento"))
+
+  let idade = anoAtual - anoNascimento
+  let tempoCarteira = anoAtual - anoEmissao
+
+  if(idade <= 20 ) {
+     return tempoCarteira >= 5 ? "passou dos 5 anos precisa renovar" : "ainda não passou os 5 anos"
+   
+  }else if(idade >= 20 || idade <= 50) {
+     return tempoCarteira >= 10 ? "passou dos 10 anos precisa renovar" : "ainda não passou os 10 anos"
+   
+  }else if(idade > 50) {
+     return tempoCarteira >=15 ? "passou dos 15 anos precisa renovar" : "ainda não passou os 15 anos"
+   
+   }else {
+       return "error"
+   }
+   
+
+}
