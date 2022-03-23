b04efe599855a6cf53aa29e4bbff3ecbfb0a583b
thaynareginam <thaynareginam@hotmail.com>
2022-03-22 21:10:13 -0300

    editando

---
+++ modulo04/projeto-labex/projeto-labex/src/pages/AdminHomePage.js
@@ -0,0 +1,209 @@
+Para e-commerces com preços competitivos, qual canal seria o mais interessante?
+Facebook
+Adwords
+Youtube
+Google Shopping
+
+O que é preciso configurar, no Google Analytics, para saber a taxa de conversão?
+Conversor
+Funil de conversão
+Meta
+Público-alvo
+
+Em relação ao usuário, o que eu NÃO consigo saber pelo Google Analytics?
+Idade
+Nome
+Região
+Canal de origem
+
+No Google Analytics, como é denominada a forma de analisar todos os canais utilizados pelos clientes que realizaram uma compra?
+Fluxo de usuários
+Funis multicanal
+Fluxo de canais
+Funis de compra
+
+Qual o limite de texto para anúncios e posts patrocinados no Facebook?
+10% da imagem
+20% da imagem
+25% da imagem
+30% da imagem
+
+Qual a vantagem de se investir em Adwords?
+Melhorar o ranqueamento orgânico do site
+Contar com ROI superior ao das demais opções de anúncio na web
+Possibilidade de segmentar campanhas por nível de escolaridade
+Facilidade e agilidade para criar campanhas
+
+Qual é o elemento mais importante e indispensável de um site, para aumento da taxa de conversão?
+CPC
+CTA
+CPM
+W3C
+
+Qual(is) fator(es) melhor sinaliza(m) que o design, a usabilidade ou o conteúdo do seu site precisa de melhorias?
+Alta taxa de rejeição e baixa conversão
+Velocidade de carregamento das páginas
+Número de visitas diárias baixo
+Site não aparecendo na 1ª página do Google
+
+Qual seria o melhor canal para abordar um público com alta intenção de compra na web, prevendo resultados a médio e longo prazos?
+SEO
+Adwords
+Facebook
+Google Display
+
+Como descobrir qual é a melhor versão de um conteúdo, com foco no aumento da taxa de conversão?
+Teste de usabilidade
+Teste de conversão simétrico
+Teste de W3C
+Teste A/B
+
+É uma prática desaconselhável para a construção de uma lista de e-mails:
+Oferecer uma recompensa gratuita
+Comprar mailing de terceiros
+Segmentar conteúdo de acordo com o público
+Oferecer conteúdo com apelo irresistível para os objetivos do público
+
+“Esquentar a lista” significa:
+Aumentar o número de leads
+Preparar a lista para e-mails de vendas
+Aumentar a taxa de abertura
+Melhorar a taxa de conversão
+
+Em termos de definição de palavras-chave, qual a forma correta de iniciar um trabalho de SEO?
+Utilizar as palavras-chave com maior volume de buscas
+Utilizar as palavras-chave com maiores lances no Adwords
+Utilizar palavras-chave com bom volume de buscas, mas com concorrência baixa ou média
+Focar em palavras-chave nas quais os seus concorrentes estão bem ranqueados
+
+O que não pode ser feito no Instagram?
+Frases curtas e objetivas
+Linguagem pessoal e íntima
+Publicidade paga
+Link clicável no post
+
+O que pode dificultar a visualização do conteúdo de um e-mail marketing?
+Conteúdo somente em imagem
+Conteúdo com links externos
+Conteúdo em HTML
+Conteúdo somente em texto
+
+Entre as opções abaixo, qual NÃO é uma vantagem de se utilizar o Youtube?
+Aumentar a relevância do vídeo para os mecanismos de busca
+Usufruir de hospedagem e tráfego deste canal
+Estar em uma poderosa ferramenta de busca
+Inserir vídeos com qualquer tipo de conteúdo
+
+Qual a principal forma de garantir uma boa taxa de cliques de abertura em um e-mail?
+Criar um design atraente
+Estruturar o e-mail somente em linguagem HTML
+Usar texto em caixa alta no título da mensagem
+Criar um título claro e objetivo
+
+Qual destas opções é uma mídia, mas não uma rede social?
+Facebook
+LinkedIn
+Ello
+Twitter
+
+Entre as opções abaixo, qual a forma menos eficiente para abordar a Geração Y (pessoas nascidas entre 1985 e 2000)?
+E-mail marketing
+Mídias sociais
+SEO
+E-commerce
+
+Qual ferramenta abaixo você utilizaria para identificar erros no site que prejudicam o ranqueamento, a exemplo de títulos duplicados?
+Google Analytics
+Google Adwords
+Google Webmasters Tools
+Google Trends
+
+Qual prática geralmente prejudica a conversão?
+Formulários com vários campos
+Textos curtos
+Imagens de pessoas
+Botão de cor diferente do restante do site
+
+Internamente ao site, qual é a principal diretriz do Google para que seu site fique bem ranqueado organicamente?
+Utilizar suas palavras-chave na meta tag keywords
+Ter um layout agradável e original
+Ter muito texto e um layout agradável
+Criar conteúdo relevante e original
+
+Qual das opções abaixo NÃO é relevante para aumentar a autoridade do domínio?
+Fazer campanhas no Adwords utilizando suas palavras-chave
+Investir em link building interno e externo
+Ter conteúdo relevante dentro do site
+Ter conteúdo que gere engajamento nas mídias sociais
+
+Qual mídia social permite direcionar anúncios e posts patrocinados por região, interesses e nível de escolaridade?
+WhatsApp
+Instagram
+Twitter
+Facebook
+
+Em qual destas mídias sociais ainda não é possível anunciar no Brasil?
+Youtube
+Facebook
+Instagram
+LinkedIn
+
+O que significa CTR?
+Quantidade de cliques num anúncio
+Valor a ser pago pelo anunciante a cada clique num anúncio
+Relação entre a quantidade de cliques e impressões de um anúncio
+Custo a ser pago pelo anunciante a cada mil impressões do anúncio
+
+Qual das opções abaixo é a mais importante no HTML, para que um site tenha relevância orgânica no Google?
+Inserir as palavras-chave que deseja ranquear na imagem principal, title, meta keywords e URL
+Inserir as palavras-chave que deseja ranquear nas meta keywords e meta description
+Inserir as palavras-chave que deseja ranquear no title, URL, tag h1 e no texto
+Inserir as palavras-chave que deseja ranquear no rodapé e URL
+
+Para quem é aconselhável investir em anúncios no Linkedin?
+Empresas cujos produtos têm alto valor agregado e foco B2B
+Empresas com foco em vendas para o varejo
+Empresas com margem de lucro reduzida e preços competitivos
+Empresas que desejam trabalhar com menor CPC
+
+Como mensurar a taxa de conversão?
+Dividindo o número de pessoas que fizeram determinada ação pelo número total de visitas
+Dividindo a taxa de rejeição pelo número total de pessoas que fizeram determinada ação
+Dividindo o número total de visitas pelo número de pessoas que fizeram determinada ação
+Dividindo o número total de visitas pelo tempo médio de permanência no site
+
+O Google+ se tornou uma rede social importante, pois:
+Possibilita maior interação com o usuário
+Ganhou alta relevância para o ranqueamento orgânico
+Tem uma capacidade maior de viralização
+Oferece ferramentas completas para análise de público
+
+Em que consiste uma rede social horizontal?
+Uma rede em que qualquer pessoa pode criar um perfil
+Uma rede de caráter corporativo, destinado a exibir a hierarquia da empresa
+Uma rede voltada a um grupo específico
+Uma rede com foco em SEO
+
+Qual é a média de conversão geral para e-commerces?
+1 a 3%
+10 a 15%
+17 a 19%
+25 a 40%
+
+Qual a fórmula para calcular o engajamento de uma fan page no Facebook?
+Dividindo o total de fãs pelo número total de usuários “falando sobre isso”
+Multiplicando o total de fãs pelo número total de usuários “falando sobre isso”
+Dividindo o número total de usuários “falando sobre isso” pelo total de fãs
+Multiplicando o total de fãs pela quantidade de “curtidas” da última semana
+
+Qual a melhor alternativa para criar um funil de conversão através de uma campanha de e-mail marketing?
+Enviar um e-mail promocional por dia
+Enviar apenas um e-mail por mês, para não estressar a lista
+Automatizar processos de envio, através de auto-responders
+De maneira alguma enviar e-mails tentando vender produtos ou serviços
+
+Para oferecer um produto único e com apelo de venda, qual seria a alternativa mais interessante?
+Hotsite
+E-commerce
+Landing page
+Portal
