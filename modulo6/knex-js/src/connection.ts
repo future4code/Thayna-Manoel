3b5f1e44c3fb71316d6926a158071788b6c71385
thaynareginam <thaynareginam@hotmail.com>
2022-04-20 02:37:47 -0300

    fazendo exercicios

--- modulo6/knex-js/src/connection.ts
+++ modulo6/knex-js/src/connection.ts
@@ -1,14 +1,17 @@
 import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const connection = knex({
 client:"mysql",
 connection: {
-    host:"35.226.146.116",
-    port:3306,
-    user:"419996-thayna-manoel",
-    password:"twEj$49hXDPCh#@22qvS",
-    database:"joy-419996-thayna-manoel",
-    multipleStatements: true
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
+  multiStatements: true
   }
 })
  
--- modulo6/knex-js/src/index.ts
+++ modulo6/knex-js/src/index.ts
@@ -1,24 +1,70 @@
 
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import connection from './connection';
+import { AddressInfo } from "net";
 
 const app = express();
 
 app.use(express.json()); 
 app.use(cors()); 
 
+// //Endpoint teste
+// app.get('/teste',(req: Request, res: Response): void => {
+//    try {
+//       res.status(200).send({message: "sucesso"});
+//   }   catch (error : any) {
+//      res.status(500).send(error.sqlMessage || error.message);
+//   }
+// });
+
+//1a) permite enviarmos uma query para o banco de dados de alguma forma
+
+//1b) Faça uma função que busque um ator pelo nome
+
+const getActorByName = async (name: string): Promise<any> => {
+   const result = await connection.raw(`
+   SELECT * FROM Actor WHERE name = '${name}'
+   `)
+
+   return result[0]
+}
+getActorByName("Tony Ramos")
+   .then(result => {
+       console.log(result)
+   })
+   .catch(err => {
+       console.log(err)
+   })
+
+   //1c) Faça uma função que receba um gender retorne a 
+   //quantidade de itens na tabela Actor com esse gender. Para atrizes, 
+   //female e para atores male.
 
 
+   const countGender = async (gender: string): Promise<any> => {
+      const result = await connection.raw(`
+      SELECT COUNT(*) FROM Actor WHERE gender = '${gender}';
+      `)
+      return result[0]
+  }
+  countGender("female")
+      .then(result => {
+          console.log(result)
+      })
+      .catch(err => {
+          console.log(err)
+      })
+  
 
-app.get('/', (request: Request, response: Response) => {
 
-response.send('Hello,word')
+//a) Uma função que receba um salário e um id e realiza a atualização do salário do ator em questão
 
-});
-
-
-
-
-app.listen(3003, () => {
-   console.log('Server rodando...');
+const server = app.listen(3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`server is running in https://localhost:${address.port}`);
+   } else {
+      console.error(`server is not running in https://localhost`);
+   }
 });
